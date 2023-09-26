8ab4c406c176694f2336142978f55ead924df1ed
bobjones12131 <bobjones12131@none.example>
2023-09-26 11:04:21 -0500

    add default_board to BoardDefaults

--- src/board_default.rs
+++ src/board_default.rs
@@ -2,12 +2,39 @@ use crate::{board::Board, piece::Piece};
 
 pub trait BoardDefaults<MyPiece: Piece<MyPiece>> {
     fn empty_board(width: usize, height: usize) -> Board<MyPiece> {
-        let thegrid: Vec<Vec<MyPiece>> = vec![vec![MyPiece::NONE; width]; height]; // row oriented
+        // Replace MyPiece with the actual piece.
+        let board: Vec<Vec<MyPiece>> = vec![vec![MyPiece::NONE; width]; height]; // row oriented
 
         Board {
-            board: thegrid,
+            board,
             width,
             height,
+        }
+    }
+    fn default_board() -> Board<MyPiece> {
+        /*
+        Type aliases, for example:
+        type MP = MyPiece;
+        type MPT = MyPieceTeam
+        type MPTy = MyPieceType
+        type XX = MP(MPT::None);
+        type WP = MP(MPT::White(MPTy::Pawn))
+        */
+
+        /*
+        Your board, for example:
+        [
+        [XX,XX,XX,XX],
+        [XX,XX,XX,XX],
+        [WP,WP,WP,WP],
+        ]
+        */
+        let board = todo!();
+        // Put the amount of rows in height and the amount of columns in width
+        Board {
+            board,
+            width: todo!(),
+            height: todo!(),
         }
     }
 }
