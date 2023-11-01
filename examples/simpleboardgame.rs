25e4635898490735322a8868867bcddec071c2a0
bobjones12131 <bobjones12131@none.example>
2023-11-01 00:03:30 -0500

    Change version from 0.3.0 to 0.0.1 and change name from easychess-lib to easychess

--- examples/simpleboardgame.rs
+++ examples/simpleboardgame.rs
@@ -16,16 +16,29 @@ fn main() {
         println!("Put in the coordinates of the piece and where you want it to go. e.g. '4,5 5,4'");
         let mut x = String::new();
         match io::stdin().read_line(&mut x) {
-            Err(_) => todo!(),
-            _ => {}
+            Ok(n) => {
+                println!("{n} bytes read");
+                println!("{x}");
+            }
+            Err(error) => {
+                println!("error: {error}");
+                continue;
+            }
         }
+        //io::stdin().read_line(&mut x).unwrap_or({
+        //    println!("aaaaa");
+        //    continue;
+        //});
         let x = x.chars().collect::<Vec<char>>();
 
         let get = |index: usize| -> Option<usize> { Some(x.get(index)?.to_digit(10)? as usize) };
 
-        let x: Option<((usize, usize), (usize, usize))> =
-            (|| -> Option<((usize, usize), (usize, usize))> {
+        let x: ((usize, usize), (usize, usize)) = //Option<((usize, usize), (usize, usize))> =
+            (|| {
                 Some(((get(0)?, get(2)?), (get(4)?, get(6)?)))
-            })();
+            })()
+            .unwrap_or({println!("abbbaaaa"); continue});
+        println!("workyturkey: {:?}", x);
+        board.move_piece(x.0, x.1);
     }
 }
